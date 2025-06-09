235fa83eafa3b60402ad745c3414ce1e16a9c3ab
Benedikt Hübschen <b.huebschen@vhmhv.de>
2025-06-09 21:57:25 +0200

    sync

--- custom_components/dreame_mower/dreame/device.py
+++ custom_components/dreame_mower/dreame/device.py
@@ -5581,7 +5581,7 @@ class DreameMowerDeviceStatus:
         if response and "CMS" not in response:
             _LOGGER.warning(
                 "DreameMowerDeviceStatus.lensbrush_life CMS missing in response: %s", response)
-            return 100  # return some fake lensbrush life in percent
+            return 30000  # return some fake lensbrush life
         return 30000 - self._get_property(DreameMowerProperty.LENSBRUSH_LEFT)['CMS'][0]
 
     @property
